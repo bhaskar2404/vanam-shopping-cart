d489cffa6601e7be2056feec3ccadf376ad31d5a
bhaskar2404 <46922900+bhaskar2404@users.noreply.github.com>
2022-03-17 21:58:49 +0530

    Product componet

---
+++ src/componets/Products.js
@@ -0,0 +1,27 @@
+import React from "react";
+import formatCurrency from "../util";
+
+const Products = ({ products }) => {
+  return (
+    <div>
+      <ul className="products">
+        {products.map((product) => (
+          <li key={product._id}>
+            <div className="product">
+              <a href={`# + ${product._id}`}>
+                <img src={product.image} alt={product.title} />
+                <p>{product.title}</p>
+              </a>
+              <div className="product-price">
+                {formatCurrency(product.price)}
+              </div>
+              <button className="button primary">Add To Cart</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
